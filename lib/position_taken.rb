49b515f0edcdd92529c49d777230df9496d9c6d4
Cecilia Moody <cecimoody@icloud.com>
2020-02-11 18:54:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,9 @@ def display_board(board)
 end
 
 def position_taken?(board,index)
-  board[index] = !(" " || "")
-  
+  board[index] = !(" " || " ")
+end
+
+def position_taken?(board,index)
+  board[index] = ("X" || "O")
 end
